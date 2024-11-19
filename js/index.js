044d06e2d4d30cbdec37fa822c7e43ae24a319b7
joaco683 <joaquinsendon@gmail.com>
2024-11-19 20:42:24 -0300

    complete el index, modifique el css y el .js

--- js/index.js
+++ js/index.js
@@ -12,3 +12,40 @@ formBuscador.addEventListener("submit", function () {
     }
 });
 
+
+const listaRecetas = document.querySelector(".listaRecetas");
+const cargarMas = document.querySelector(".cargarMas");
+let offset = 0;
+
+function cargarRecetas() {
+    const url = `https://dummyjson.com/recipes?skip=${offset}&limit=10`;
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.recipes);
+            let contenido = "";
+
+            for (let i = 0; i < data.recipes.length; i++) {
+                console.log(data.recipes[i]);
+                contenido += `<article class="articuloHome" >
+                    <img class="imagenHome" src="${data.recipes[i].image}" alt="">
+                    <h3>${data.recipes[i].name}</h3>
+                    <p>Nivel de Dificultad: ${data.recipes[i].difficulty}</p>
+                    <a href="receta.html?id=${data.recipes[i].id}">Ver Detalle</a>
+                </article>`;
+            }
+
+            listaRecetas.innerHTML += contenido; 
+            offset += 10;
+        })
+        .catch(function (error) {
+            console.log("El error es: " + error);
+        });
+}
+
+cargarMas.addEventListener("click", cargarRecetas);
+
+cargarRecetas();
