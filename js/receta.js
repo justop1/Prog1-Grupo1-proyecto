51c41cde67a98bcd82f32f50cd290fee2d1ec613
Guerzo1 <sguerzoni@udesa.edu.ar>
2024-11-28 00:06:57 -0300

    Valide el formulario register, cambie el estilo de receta.html, su responsive y algunos cambios mas en css

--- js/receta.js
+++ js/receta.js
@@ -6,39 +6,45 @@ const recipeId = params.get("id");
 
 
 if (recipeId) {
-   const url = `https://dummyjson.com/recipes/${recipeId}`;
+    const url = `https://dummyjson.com/recipes/${recipeId}`;
 
 
 
 
-   fetch(url)
-       .then(function (response) {
-           return response.json();
-       })
-       .then(function (data) {
-           recetaDetalle.innerHTML = `
-              <article>
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            recetaDetalle.innerHTML = `
+              <article class="receta1">
+                  
+                  <div class="infoR">
                   <h2>${data.name}</h2>
-                  <img class="imgReceta" src="${data.image}" alt="">
                   <p><strong>Instrucciones:</strong> ${data.instructions}</p>
                   <p><strong>Tiempo de cocción:</strong> ${data.cookTimeMinutes} minutos</p>
-                  <p><strong>Categorías:</strong></p><div>
+                  <p><strong>Categorías:</strong></p>
+                 
                    ${function () {
-                   let categoriasHTML = "";
-                   for (let i = 0; i < data.tags.length; i++) {
-                       categoriasHTML += `<a class="nombreCategorias" href="category.html?categoria=${data.tags[i]}">${data.tags[i]}</a> `;
-                   }
-                   return categoriasHTML;
-               }()}
-               </div>
+                    let categoriasHTML = "";
+                    for (let i = 0; i < data.tags.length; i++) {
+                        categoriasHTML += `<a class="nombreCategorias" href="category.html?categoria=${data.tags[i]}">${data.tags[i]}</a> `;
+                    }
+                    return categoriasHTML;
+                }()}
+                 </div>
+                
+                  <div class="imagenR">
+                  <img class="imgReceta" src="${data.image}" alt="">
+                  </div>
 
 
               </article>`;
-       })
-       .catch(function (error) {
-           console.error("Error al cargar la receta:", error);
-           recetaDetalle.innerHTML = `<p>Error al cargar la receta. Intenta nuevamente más tarde.</p>`;
-       });
+        })
+        .catch(function (error) {
+            console.error("Error al cargar la receta:", error);
+            recetaDetalle.innerHTML = `<p>Error al cargar la receta. Intenta nuevamente más tarde.</p>`;
+        });
 } else {
-   recetaDetalle.innerHTML = `<p>No se encontró el ID de la receta en la URL.</p>`;
+    recetaDetalle.innerHTML = `<p>No se encontró el ID de la receta en la URL.</p>`;
 }
--- js/register.js
+++ js/register.js
@@ -1,8 +1,26 @@
 let boton = document.querySelector('button');
+let email = document.querySelector("#email");
+let formLogin = document.querySelector(".formLogin");
+let clave = document.querySelector("#contrasennia");
+let obligatorio = document.querySelector(".obligatorio")
+let obligatorio2 = document.querySelector(".obligatorio2")
 
 boton.addEventListener('mouseover', function () {
     this.style.color = '#ffffff';
 });
 boton.addEventListener("mouseout", function () {
     this.style.color = "#000000";
+});
+
+formLogin.addEventListener("submit", function (event) {
+  
+    if (email.value == "") {
+        event.preventDefault();
+        obligatorio.innerText = "Por favor complete el campo"
+    }
+    else if (clave.value == "") {
+        event.preventDefault();
+        obligatorio2.innerText = "Por favor complete el campo"
+    }
+   
 });
